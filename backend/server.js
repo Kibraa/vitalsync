f1d5673da787bca64fc14cfa0b9c16147deb8a98
Ibrahim <karamanlian.ibrahim@gmail.com>
2026-04-01 14:45:17 +0200

    feat: ajout de l'endpoint /api/stats

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,8 @@ app.get("/api/vitals", (req, res) => {
   res.json([]);
 });
 
+app.get("/api/stats", (req, res) => {
+  res.json({ users: 42, active: 18 });
+});
+
 app.listen(3000, () => console.log("VitalSync API on :3000"));
